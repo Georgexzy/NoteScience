3836ea439f60a4e81219b22be0fb2b93ba24ffb3
Georgexzy <93133581+Georgexzy@users.noreply.github.com>
2022-05-19 17:43:48 +0100

    NoteScience V2
    -Added Text Font Selector

--- NoteScience.py
+++ NoteScience.py
@@ -1,15 +1,45 @@
+
+
+
+#Importing Desired Modules
+
 from cProfile import label
 from cgitb import text
 from struct import pack
 from tkinter import *
 import tkinter as tk
-from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter import font
+from tkinter.filedialog import *
 from tkinter.font import Font
 from tkinter import font as tkFont
-from tkinter import messagebox
+from tkinter import colorchooser
+from tkinter.messagebox import showerror
+from turtle import clear
+from typing import List    
+
+
 root = tk.Tk()
 
 
+root.configure(background='black')
+
+our_font = font.Font(family="Helvetica", size="32")
+
+
+my_listbox = Listbox(root, selectmode=SINGLE, width=275, height = 2)
+my_listbox.pack()
+
+for fnt in font.families():
+    my_listbox.insert('end', fnt)
+
+
+def font_chooser(event):
+    our_font.config(
+        family=my_listbox.get(my_listbox.curselection()))
+
+
+
+my_listbox.bind('<ButtonRelease-1>', font_chooser)
 
 
 
@@ -20,18 +50,9 @@ root = tk.Tk()
 
 
 
+filename = "Untitled"
 
-
-
-
-
-
-
-
-
-
-
-filename = None
+#Defining Functions
 
 def newFile():
     global filename
@@ -41,7 +62,7 @@ def newFile():
 def saveFile(): 
     global filename
     t = text.get(0.0,END)
-    f = open (filename,"w")
+    f = open(filename + '.txt', 'w')
     f.write(t)
     f.close()
 
@@ -51,7 +72,8 @@ def saveAs():
     try:
         f.write(t.rstrip())
     except:
-            showerror(title="Oops!", message="Unable to save file...")
+        showerror(tile="Oops!", message="Unable to save file...")
+
 
 def openFile():
  f = askopenfile(mode='r')
@@ -59,55 +81,105 @@ def openFile():
  text.delete(0.0, END)
  text.insert(0.0,t)
 
-def add_highlighter():
-      text.tag_add("start", "1.0","end")
-      text.tag_configure("start",background="White", foreground= "Red")
-
-
-def remove_highlighter():
-      text.tag_add("start", "1.0","end")
-      text.tag_configure("start", background="White", foreground= "Black")
+def bolder():
+    bold_font = font.Font(text, text.cget("font"))
+    bold_font.configure(weight="bold")
+  
+    text.tag_config("bold", font = bold_font)
+    current_tags = text.tag_names("sel.first")
     
-   
+    if "bold" in current_tags:
+        text.tag_remove("bold", "sel.first", "sel.last")
+    else:
+          text.tag_add("bold", "sel.first", "sel.last")
+    
+def italicsiser():
+    italics_font = font.Font(text, text.cget("font"))
+    italics_font.configure(slant="italic")
+
+    text.tag_configure("italics", font= italics_font)
+    current_tags = text.tag_names("sel.first")
+     
+    if "italics" in current_tags:
+        text.tag_remove("italics", "sel.first", "sel.last")
+    else:
+          text.tag_add("italics", "sel.first", "sel.last") 
+
+def text_colour():
+    my_colour = colorchooser.askcolor()[1]
 
 
-def darkmode():
-      if darkmode.get() == 1:
-        root.config(background='black')
+    colour_font = font.Font(text, text.cget("font"))
+    text.tag_configure("coloured", font=colour_font, foreground=my_colour)
+    current_tags = text.tag_names("sel.first")
+     
+    if "coloured" in current_tags:
+        text.tag_remove("coloured", "sel.first", "sel.last")
+    else:
+          text.tag_add("coloured", "sel.first", "sel.last") 
 
-      elif darkmode.get() == 0:
-        root.config(background='white')
-      else:
-        messagebox.showerror('PythonGuides', 'Something went wrong!')
+def background_colour():
+     my_colour = colorchooser.askcolor()[1]
+     if my_colour:
+         text.config(bg=my_colour)
+
+def all_text_colour():
+    my_colour =colorchooser.askcolor()[1]
+    if my_colour:
+        text.config(fg=my_colour)
+
+def select_all():
+    text.tag_add('sel', '1.0', 'end')
+
+def clear_all():
+    text.delete(1.0, END)
 
 
 
-darkmode = BooleanVar()
-darkmode.set(False)
+scroll_bar = tk.Scrollbar(root)
+scroll_bar.pack(side=tk.RIGHT)
+
+
+
+
+#Creating the window
 
 root.title("NoteScience Text Editor")
-root.geometry("400x400")
-text = Text ( root, height = 40)
+root.geometry("720x500")
+text = Text ( root, height = 50, width = 100, undo=True, wrap="word", font=our_font)
 text.pack()
 
-Desired_font = tk.font.Font( family = "Times New Roman", 
-                                 size = 13, 
-                                 weight = "bold")
-text.configure(font = Desired_font)
 
 
 
 
-
+#Menu Stuff
 menubar = Menu(root)
 
+
+
+
 viewmenu = Menu(menubar)
-menubar.add_cascade(label="View",menu=viewmenu, underline=0)
-viewmenu.add_checkbutton(label='darkmode', onvalue=1, offvalue=0, variable=darkmode, command=darkmode )
+menubar.add_cascade(label="View",menu=viewmenu)
+viewmenu.add_command(label="Bold", command=bolder)
+viewmenu.add_command(label="Italics", command=italicsiser)
+viewmenu.add_separator()
+viewmenu.add_command(label= "Text Colour", command=text_colour)
+viewmenu.add_command(label="Background Colour", command=background_colour)
+viewmenu.add_command(label="All Text Colour", command=all_text_colour)
+viewmenu.add_separator()
+viewmenu.add_command(label="Select all", command=select_all)
+viewmenu.add_command(label="Clear", command=clear_all )
 
 
-viewmenu.add_command(label="Highlight", command= add_highlighter)
-viewmenu.add_command(label="Undo Highlight", command=remove_highlighter)
+
+
+
+
+menubar.add_command(label="Undo", command=text.edit_undo)
+menubar.add_command(label="Redo", command=text.edit_redo)
+ 
+
 
 
 
@@ -121,6 +193,13 @@ filemenu.add_separator()
 filemenu.add_command(label="Quit", command=root.quit)
 menubar.add_cascade(label="File",menu=filemenu)
 root.config(menu=menubar)
+
+
+
+
+
+
+
 
 
 root.mainloop()
